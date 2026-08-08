f2d752fb2687bc3161bd65c4bd49bfb38ed9da24
wandl-6A72h <hiwepy@gmail.com>
2026-08-08 23:58:41 +0800

    chore: propagate pom standardization, tests, javadoc and @author fix from 4.1.x
    - Add jacoco-maven-plugin 0.8.15 with 90% coverage gate
    - Add central-publishing-maven-plugin 0.11.0
    - Update surefire (enable tests, argLine for JaCoCo, *_Test.java pattern)
    - Fix SCM URLs
    - Copy test files from 4.1.x branch
    - Copy Javadoc improvements from 4.1.x branch
    - Standardize @author to [@Loong Wan](https://github.com/loong10k)
    - Propagated to 3.5.x branch

--- src/main/java/com/alibaba/otter/canal/spring/boot/CanalThreadPoolProperties.java
+++ src/main/java/com/alibaba/otter/canal/spring/boot/CanalThreadPoolProperties.java
@@ -25,7 +25,7 @@ import java.util.function.Function;
 
 /**
  *
- * @author ： <a href="https://github.com/hiwepy">hiwepy</a>
+ * @author [@Loong Wan](https://github.com/loong10k)
  */
 @ConfigurationProperties(CanalThreadPoolProperties.PREFIX)
 @Data
