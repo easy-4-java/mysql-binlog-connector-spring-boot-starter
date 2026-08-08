1c2853378bb3e8cb992a8de56a246a45d170b38b
wandl-6A72h <hiwepy@gmail.com>
2026-08-08 23:58:47 +0800

    chore: propagate pom standardization, tests, javadoc and @author fix from 4.1.x
    - Add jacoco-maven-plugin 0.8.15 with 90% coverage gate
    - Add central-publishing-maven-plugin 0.11.0
    - Update surefire (enable tests, argLine for JaCoCo, *_Test.java pattern)
    - Fix SCM URLs
    - Copy test files from 4.1.x branch
    - Copy Javadoc improvements from 4.1.x branch
    - Standardize @author to [@Loong Wan](https://github.com/loong10k)
    - Propagated to 4.0.x branch

--- src/main/java/com/alibaba/otter/canal/util/CanalUtils.java
+++ src/main/java/com/alibaba/otter/canal/util/CanalUtils.java
@@ -31,7 +31,7 @@ import java.util.Date;
 import java.util.List;
 
 /**
- * @author 		： <a href="https://github.com/hiwepy">hiwepy</a>
+ * @author [@Loong Wan](https://github.com/loong10k)
  */
 public class CanalUtils {
 
